cb614dd21ae643480f00761c0c69be38b4bd5363
Jen Wachter <jwachter@jhu.edu>
2023-04-12 12:08:04 -0400

    Do not show title richtext if the title toggle is off

--- src/timeline-milestone/edit.js
+++ src/timeline-milestone/edit.js
@@ -2,7 +2,6 @@ import { InnerBlocks, RichText, useBlockProps } from '@wordpress/block-editor';
 import { PanelBody, TextControl } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
-import CustomAppender from '../components/custom-appender';
 import Toggles from "../components/toggles";
 
 import './editor.scss';
@@ -17,14 +16,14 @@ export default ({ attributes, clientId, isSelected, setAttributes }) => {
         onChange={(date) => setAttributes({ date })}
         value={attributes.date}
       />
-      <RichText
+      {attributes.hasTitle && <RichText
         tagName={'h4'}
         value={attributes.title}
         multiline={ false }
         allowedFormats={[ 'core/bold', 'core/italic' ]}
         onChange={(title) => setAttributes({ title })}
         placeholder={__( 'Enter a title...' )}
-      />
+      />}
       <InnerBlocks
         allowedBlocks={['jhu/simple-wysiwyg']}
         renderAppender={false}
