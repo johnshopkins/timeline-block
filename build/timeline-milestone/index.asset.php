47dcf5d20d86e62fda7583ac6e6335a369400175
Jen Wachter <jwachter@jhu.edu>
2023-04-12 12:09:48 -0400

    bumped version

--- build/timeline-milestone/index.asset.php
+++ build/timeline-milestone/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e9f555eca633bdfe810e');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '602fd916aa91c4f9664b');
